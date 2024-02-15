2ca2d91efff6558d855061ff0b2d035c17bd39c4
Ashley Arthur <ashleyarthur98@gmail.com>
2024-02-15 23:08:09 +0000

    WIP: New AsyncIO

--- engineio/src/engine.rs
+++ engineio/src/engine.rs
@@ -40,14 +40,72 @@ pub(crate) enum EngineOutput {
 
 #[derive(Debug, Clone)]
 pub(crate) enum Transport {
-    Polling { active:Option<(Instant,Duration)> },
+    Polling(PollingState),
     Continuous
+}
+
+impl Transport {
+    pub(crate) fn poll_state(&mut self) -> Option<&mut PollingState> {
+        match self {
+            Transport::Polling(p) => Some(p),
+            _ => None
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub(crate) struct PollingState {
+    pub active: Option<(Instant,Duration)>,
+    pub count: u64
+}
+
+impl Default for PollingState {
+    fn default() -> Self {
+        return Self {
+            active:None,
+            count:0
+        }
+    }
+}
+
+impl PollingState {
+    pub fn activate_poll(&mut self, start:Instant, max_length:Duration) {
+        // TODO: We need to ensure min length > heartbeat
+        let length = if self.count > 0 { Duration::from_millis(100) } else { max_length };
+        self.active = Some((start,length));
+        self.count = 0;
+    }
+
+    pub fn update_poll(&mut self, now:Instant) {
+        if let Some((start,length)) = self.active {
+            if now > start + length { self.active = None; self.count = 0; }
+        }
+    }
+
+    pub fn increase_count(&mut self) {
+        if self.count == 0 {
+            if let Some((s,length)) = self.active {
+                self.active = Some((s,Duration::from_millis(500)));
+            }
+        }
+        self.count += 1;
+    }
 }
 
 #[derive(Debug, Clone)]
 pub(crate) struct Heartbeat {
     pub last_seen:Instant,
     pub last_ping:Option<Instant>
+}
+
+impl Heartbeat {
+    pub fn seen_at(&mut self, at:Instant){
+        self.last_seen = at;
+        self.last_ping = None;
+    }
+    pub fn pinged_at(&mut self, at:Instant){
+        self.last_ping = Some(at);
+    }
 }
 
 #[derive(Debug, Clone)]
--- engineio/src/server.rs
+++ engineio/src/server.rs
@@ -1,5 +1,5 @@
 use std::{collections::VecDeque, time::{Instant, Duration}};
-use crate::engine::{
+use crate::{engine::{
     Heartbeat,
     Transport,
     EngineIOClientCtrls,
@@ -9,7 +9,7 @@ use crate::engine::{
     EngineKind,
     EngineCloseReason,
     EngineError
-};
+}, PollingState};
 use crate::proto::{Payload, TransportConfig, Sid};
 
 #[derive(Debug)]
@@ -36,29 +36,26 @@ impl ConnectedState {
             return Self (t, Heartbeat { last_seen: now, last_ping: None }, config.unwrap_or(TransportConfig::default()))
         }
 
-        fn update_last_seen(mut self, now:Instant) -> Self {
-            self.1.last_seen = now;
-            self.1.last_ping = None;
-            return self
+        fn update(mut self, f:impl Fn(&mut Transport, &mut Heartbeat) -> ()) -> Self {
+            f(&mut self.0, &mut self.1);
+            self
         }
 
-        fn update_new_ping(mut self, now:Instant) -> Self {
-            self.1.last_ping = Some(now);
-            return self
-        }
-
-        fn update_new_poll(mut self, now:Instant, length:Duration) -> Self {
-            match self.0 {
-                Transport::Continuous => {},
-                Transport::Polling{ .. } => { self.0 = Transport::Polling{ active:Some((now, length)) }; }
-            };
-            return self
+        fn replace(mut self, f:impl Fn(Transport,Heartbeat) -> (Transport, Heartbeat)) -> Self {
+            let (t,h) = f(self.0, self.1);
+            return ConnectedState (t,h,self.2)
         }
 
         fn set_config(mut self, config:TransportConfig) -> Self {
             self.2 = config;
             return self
         }   
+}
+
+impl Default for ConnectedState {
+    fn default() -> Self {
+        return Self(Transport::Continuous, Heartbeat { last_seen: Instant::now(), last_ping: None }, TransportConfig::default())
+    }
 }
 
 #[derive(Debug)]
@@ -84,7 +81,7 @@ impl EngineIOServer {
         let next_state = match &input {
             EngineInput::Data(_) => {
                 match &self.state {
-                    EngineState::Connected(..) => Ok(None),
+                    EngineState::Connected(state) => Ok(Some(EngineState::Connected(state.clone().update(|t,_| if let Transport::Polling(p) = t { p.increase_count(); })))),
                     _ => Err(EngineError::AlreadyClosed)
                 }
             },
@@ -114,11 +111,11 @@ impl EngineIOServer {
                 Ok(EngineState::Closed(EngineCloseReason::Error(EngineError::UnknownPayload)))
             },
             EngineInput::Data(Ok(p)) => {
-                match &self.state {
+                match &mut self.state {
                     EngineState::Connected(state) => {
                         match p {
                             Payload::Close(..) => Ok(EngineState::Closed(EngineCloseReason::ClientClose)),
-                            _ => Ok(EngineState::Connected(state.clone().update_last_seen(now))
+                            _ => Ok(EngineState::Connected(state.clone().update(|_,heartbeat| heartbeat.seen_at(now)))
 )
                         }
                     },
@@ -130,9 +127,14 @@ impl EngineIOServer {
             EngineInput::Control(EngineIOClientCtrls::Poll) => {
                 match &self.state {
                     EngineState::Connected(s) => {
-                        match s.0 {
-                            Transport::Polling { active:Some(..) } => Ok(EngineState::Closed(EngineCloseReason::Error(EngineError::InvalidPoll))),
-                            Transport::Polling { active:None } => Ok(EngineState::Connected(s.clone().update_new_poll(now, Duration::from_secs(1)))),
+                        match &s.0 {
+                            Transport::Polling(PollingState { active:Some(..), ..}) => Ok(EngineState::Closed(EngineCloseReason::Error(EngineError::InvalidPoll))),
+                            Transport::Polling(PollingState{ active:None, ..}) => {
+                                Ok(EngineState::Connected(s.clone().update(|t,h| {
+                                    h.seen_at(now);
+                                    t.poll_state().map(|p| p.activate_poll(now, Duration::from_millis(s.2.ping_timeout)));
+                                })))
+                            },
                             Transport::Continuous => Ok(EngineState::Closed(EngineCloseReason::Error(EngineError::InvalidPoll)))
                         }
                     },
@@ -148,7 +150,7 @@ impl EngineIOServer {
                 match &self.state {
                     EngineState::New{ .. }  => { 
                         let t = match kind {
-                            EngineKind::Poll => Transport::Polling { active: None },
+                            EngineKind::Poll => Transport::Polling(PollingState::default()),
                             EngineKind::Continuous => Transport::Continuous
                         };
                         Ok(EngineState::Connected(ConnectedState::new( t, *config, now)))
@@ -175,49 +177,45 @@ impl EngineIOServer {
     pub fn poll_output(&mut self, now:Instant) -> Option<EngineOutput> {
         // We have to push forward the 'time' element of state so we can trigger timeouts
         let next = match &self.state {
-            EngineState::Connected(ConnectedState(t,h,c)) => {
-                let next = match t {
-                    Transport::Polling { active:Some((start,length)) } =>  {
-                        if now >= *start+*length {
-                            Some(ConnectedState(Transport::Polling { active: None }, h.clone(), c.clone()))
-                        }
-                        else {
-                            None
-                        }
-                    },
-                    _  => None
-                }.unwrap_or(ConnectedState(t.clone(),h.clone(),c.clone()));
-
-                let next = match h.last_ping {
-                    None => if now > h.last_seen + Duration::from_millis(c.ping_interval) { 
-                        EngineState::Connected(next.update_new_ping(now)) } else { EngineState::Connected(ConnectedState(t.clone(),h.clone(),c.clone())) 
-                    },
-                    Some(start) => if now > start + Duration::from_millis(c.ping_timeout) { 
-                        EngineState::Closed(EngineCloseReason::Timeout) } else { EngineState::Connected(ConnectedState(t.clone(),h.clone(),c.clone()))
-                    }
-                };
-                Some(next)
+            EngineState::Connected(ConnectedState(_, Heartbeat { last_ping:Some(i), .. }, config)) if now > *i + Duration::from_millis(config.ping_timeout) => {
+                Some(EngineState::Closed(EngineCloseReason::Timeout))
             },
-            _ => None
+            EngineState::Connected(state) => {
+                Some(EngineState::Connected(state.clone().update(|transport,heartbeat| {
+                    if let None = heartbeat.last_ping {
+                        if now > heartbeat.last_seen + Duration::from_millis(state.2.ping_interval) { heartbeat.pinged_at(now) };
+                    }
+                    if let Transport::Polling(p@PollingState { active:Some(..), .. }) = transport {
+                        p.update_poll(now)
+                    }
+                })))
+            },
+            EngineState::New { start_time } if now > *start_time + Duration::from_secs(5) => Some(EngineState::Closed(EngineCloseReason::Timeout)),
+            EngineState::New { .. } => None,
+            EngineState::Closed(_) => None
         };
+        if let Some(next) = next { self.update_state(next); };
 
-        if let Some(new_state) = next { self.update_state(new_state) } 
         self.output.pop_front().or_else(||{
             match &self.state {
                 EngineState::New { start_time } => Some(EngineOutput::Pending(now - *start_time + Duration::from_secs(5))),
                 EngineState::Connected(ConnectedState(t,h,c)) => {
-                    let next_poll_deadline = if let Transport::Polling { active:Some((start,length)) } = t { Some(*start + *length) } else { None };
+                    let next_poll_deadline = if let Transport::Polling(PollingState { active:Some((start,length)), count }) = t { Some(*start + *length) } else { None };
                     let next_heartbeat_deadline = if let Some(s) = h.last_ping { s + Duration::from_millis(c.ping_timeout) } else { h.last_seen + Duration::from_millis(c.ping_interval) };
 
                     let deadline = [ 
                         next_poll_deadline.map(|d| d - now ),
                         Some(next_heartbeat_deadline - now)
-                    ]
+                    ];
+
+                    dbg!(&deadline);
+
+                    let d = deadline
                     .into_iter()
                     .filter_map(|a| a)
                     .min().unwrap();
 
-                    Some(EngineOutput::Pending(deadline))
+                    Some(EngineOutput::Pending(d))
                 }
                 EngineState::Closed(..) => None
             }
@@ -229,6 +227,8 @@ impl EngineIOServer {
         dbg!((&self.state,&next_state));
         // We update state and push any additional output due to state transition 
         let io = match (&self.state, &next_state) {
+            (EngineState::Closed(_), _ ) => {},
+
             (EngineState::New {..} , EngineState::Connected(ConnectedState(t,_,config))) => {
                 let upgrades = if let Transport::Polling { .. } = t { vec!["websocket"] } else { vec![] };
                 let data = serde_json::json!({
@@ -246,14 +246,14 @@ impl EngineIOServer {
             },
 
             // Polling Start 
-            (EngineState::Connected(ConnectedState(Transport::Polling { active:None }, _,_ )),
-             EngineState::Connected(ConnectedState(Transport::Polling { active: Some(..) },_,_))) => { 
+            (EngineState::Connected(ConnectedState(Transport::Polling(PollingState { active:None, .. }), _,_ )),
+             EngineState::Connected(ConnectedState(Transport::Polling(PollingState { active:Some(..), ..}),_,_))) => { 
                 self.output.push_back(EngineOutput::Stream(true));
             }
             
             // Polling End
-            (EngineState::Connected(ConnectedState(Transport::Polling { active:Some(..)}, _,_ )),
-             EngineState::Connected(ConnectedState(Transport::Polling { active: None },_,_))) => {
+            (EngineState::Connected(ConnectedState(Transport::Polling(PollingState { active:Some(..), ..}),_,_)),
+             EngineState::Connected(ConnectedState(Transport::Polling(PollingState { active:None, .. }),_,_))) => {
                 self.output.push_back(EngineOutput::Stream(false));
             },
 
@@ -261,6 +261,7 @@ impl EngineIOServer {
             (_, EngineState::Closed(EngineCloseReason::ClientClose)) => {
                 self.output.push_back(EngineOutput::Stream(false));
             },
+
             (_, EngineState::Closed(reason)) => {
                 self.output.push_back(EngineOutput::Send(Payload::Close(reason.clone())));
                 self.output.push_back(EngineOutput::Stream(false));
@@ -277,22 +278,38 @@ impl EngineIOServer {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::Payload;
+    use crate::{Payload, MessageData};
 
     fn setup(now:Instant) -> EngineIOServer {
         return EngineIOServer { output: VecDeque::new(), session:uuid::Uuid::new_v4(), state: EngineState::New { start_time: now } }
     }
 
+    fn next_time(now:Instant, then:u64) -> Instant { now + Duration::from_millis(then) }
+
+    fn drain(now:Instant, engine:&mut EngineIOServer) -> Option<Duration> {
+        loop {
+            match engine.poll_output(now) {
+                None => break None,
+                Some(EngineOutput::Pending(d)) => break Some(d),
+                _ => {}
+            }
+        }
+    }
+
     #[test]
     fn on_poll_new() {
         let time = Instant::now();
-        let mut e =  setup(time);
+        let mut e = setup(time);
         let time_next = time +  Duration::from_millis(1);
-        e.input_recv(EngineInput::Control(EngineIOClientCtrls::New(None, EngineKind::Poll)), time);
+        let _ =  e.input_recv(EngineInput::Control(EngineIOClientCtrls::New(None, EngineKind::Poll)), time);
+
+        let out = e.poll_output(time_next);
+        let res = if let Some(EngineOutput::Stream(true)) = out { true } else { false };
+        assert!(res, "res was {out:?}");
 
         let out = e.poll_output(time_next);
         let res = if let Some(EngineOutput::Send(Payload::Open(..))) = out { true } else { false };
-        assert!(res);
+        assert!(res, "res was {out:?}");
     }
 
     #[test] 
@@ -303,12 +320,47 @@ mod tests {
         let _ = e.input_recv(EngineInput::Control(EngineIOClientCtrls::New(None, EngineKind::Continuous)), time);
 
         let out = e.poll_output(time_next);
-        let res = if let Some(EngineOutput::Send(Payload::Open(..))) = out { true } else { false };
-        assert!(res);
+        let res = if let Some(EngineOutput::Stream(true)) = out { true } else { false };
+        assert!(res, "res was {out:?}");
 
         let out = e.poll_output(time_next);
-        let res = if let None = out { true } else { false };
-        assert!(res);
+        let res = if let Some(EngineOutput::Send(Payload::Open(..))) = out { true } else { false };
+        assert!(res, "res was {out:?}");
+
+        let out = e.poll_output(time_next);
+        let res = if let Some(EngineOutput::Pending(_))= out { true } else { false };
+        assert!(res, "res was {out:?}");
     }
 
+    #[test] 
+    fn on_poll_ctrl_empty() {
+        let now = Instant::now();
+        let mut engine = setup(now);
+        let _ = engine.input_recv(EngineInput::Control(EngineIOClientCtrls::New(None, EngineKind::Poll)), now);
+        
+        let later = next_time(now, 100);
+        let _ = engine.input_recv(EngineInput::Control(EngineIOClientCtrls::Poll), later);
+        drain(now, &mut engine);
+
+        let last_seen = if let EngineState::Connected(ConnectedState(_,Heartbeat { last_seen, last_ping },_,)) = engine.state { Some(last_seen) } else { None };
+        assert!(last_seen.is_some());
+        assert!(last_seen.unwrap() == later);
+    }
+
+    #[test] 
+    fn on_poll_ctrl_pendingEvent() {
+        let now = Instant::now();
+        let mut engine = setup(now);
+        let _ = engine.input_recv(EngineInput::Control(EngineIOClientCtrls::New(None, EngineKind::Poll)), now);
+
+        engine.input_recv(EngineInput::Data(Ok(Payload::Message(MessageData::String(vec![])))), now);
+
+        let later = next_time(now, 100);
+        let _ = engine.input_recv(EngineInput::Control(EngineIOClientCtrls::Poll), later);
+        drain(now, &mut engine);
+
+        let last_seen = if let EngineState::Connected(ConnectedState(_,Heartbeat { last_seen, last_ping },_,)) = engine.state { Some(last_seen) } else { None };
+        assert!(last_seen.is_some());
+        assert!(last_seen.unwrap() == later);
+    }
 }
