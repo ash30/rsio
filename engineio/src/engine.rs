3c6845aa3cdf91699fe558b82e429ec436dfc4bf
Ashley Arthur <ashleyarthur98@gmail.com>
2024-02-19 22:46:42 +0000

    Initial Server/Client Abstraction

--- engineio/src/engine.rs
+++ engineio/src/engine.rs
@@ -1,6 +1,8 @@
 use std::time::{Instant, Duration};
 use std::fmt;
+use crate::Either;
 pub use crate::proto::*;
+use std::collections::VecDeque;
 
 #[derive(Debug,Clone,Copy)]
 pub enum EngineKind {
@@ -107,25 +109,7 @@ impl Heartbeat {
     }
 }
 
-#[derive(Debug)]
-pub(crate) enum EngineState {
-    New { start_time:Instant },
-    Connecting { start_time:Instant },
-    Connected(ConnectedState),
-    Closed(EngineCloseReason),
-}
-
-impl EngineState {
-    pub fn has_error(&self) -> Option<&EngineError> {
-        match self {
-            EngineState::Closed(EngineCloseReason::Error(e)) => Some(e),
-            _ => None
-        }
-    }
-}
-
 // =====================
-
 #[derive(Debug, Clone)]
 pub(crate) struct ConnectedState(pub Transport, pub Heartbeat, pub TransportConfig);
 
@@ -143,6 +127,102 @@ impl ConnectedState {
             self.2 = config;
             return self
         }   
+}
+
+
+// =====================
+
+#[derive(Debug)]
+pub(crate) enum EngineState {
+    New { start_time:Instant },
+    Connecting { start_time:Instant },
+    Connected(ConnectedState),
+    Closed(EngineCloseReason),
+}
+
+impl EngineState {
+    pub fn has_error(&self) -> Option<&EngineError> {
+        match self {
+            EngineState::Closed(EngineCloseReason::Error(e)) => Some(e),
+            _ => None
+        }
+    }
+}
+
+pub(crate) trait EngineStateEntity {
+    type Sender;
+    type Receiver;
+    fn time(&self, now:Instant, config:&TransportConfig) -> Option<EngineState>;
+    fn send(&self, input:EngineInput<Self::Sender>, now:Instant, config:&TransportConfig) -> Result<Option<EngineState>, EngineError>;
+    fn recv(&self, input:EngineInput<Self::Receiver>, now:Instant, config:&TransportConfig) -> Result<Option<EngineState>, EngineError>;
+    fn update(&mut self, next_state:EngineState, out_buffer:&mut VecDeque<EngineOutput>) -> &EngineState;
+    fn next_deadline(&self) -> Option<Instant>;
+}
+
+// =====================
+
+pub (crate) struct Engine<T> {
+    pub session: Sid,
+    output: VecDeque<EngineOutput>,
+    state: T,
+}
+
+impl<T> Engine<T>
+where T:EngineStateEntity {
+    pub fn new(id:Sid, now:Instant, start_state:T) -> Self {
+        return Self { 
+            session: id,
+            output: VecDeque ::new(),
+            state: start_state,
+        }
+    }
+
+    fn advance_time(&mut self, now:Instant, config:&TransportConfig) {
+        while let Some(d) = self.state.next_deadline() {
+            if now < d { break };
+            if let Some(s) = self.state.time(now, config) {
+                self.state.update(s, &mut self.output);
+            }
+        }
+    }
+
+    pub fn input(&mut self, i:Either<EngineInput<T::Sender>,EngineInput<T::Receiver>>, now:Instant, config:&TransportConfig) -> Result<(),EngineError> {
+        self.advance_time(now, config);
+        let next_state = match i {
+            Either::A(a) => self.state.send(a, now, config),
+            Either::B(b) => self.state.recv(b, now, config)
+        };
+        match next_state {
+            Err(e) => {
+                Err(e)
+            },
+            Ok(next_state) => {
+                // Buffer Data Input AND action state transitions
+                match i {
+                    Either::A(EngineInput::Data(Ok(msg))) => { self.output.push_back(EngineOutput::Send(msg)) },
+                    Either::B(EngineInput::Data(Ok(msg))) => { self.output.push_back(EngineOutput::Recv(msg)) }
+                    _ => {}
+                }
+                if let Some(s) = next_state {
+                    let e = self.state.update(s, &mut self.output).has_error();
+                    if let Some(e) = e { Err(e.clone()) } else { Ok(()) }
+                }
+                else {
+                    Ok(())
+                }
+            }
+        }
+    }
+
+    pub fn poll(&mut self,now:Instant, config:&TransportConfig) -> Option<EngineOutput> {
+        let next_state = self.state.time(now, config);
+        self.output.pop_front().or_else(||{
+            self.advance_time(now, config);
+            self.state.next_deadline().map(|d| EngineOutput::Pending(d))
+        })
+
+    }
+
 }
 
 // =====================
