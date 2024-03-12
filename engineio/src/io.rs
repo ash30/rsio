3b13461fd2a6abb37c08e8f2432b58d13f1c2edd
Ashley Arthur <ashleyarthur98@gmail.com>
2024-03-12 23:53:55 +0000

    super wip

--- engineio/src/io.rs
+++ engineio/src/io.rs
@@ -1,48 +1,67 @@
 use std::fmt::Debug;
 use std::time::Duration;
 use std::time::Instant;
-use std::collections::HashMap;
 use futures_util::TryFutureExt;
 use tokio::select;
-use tokio::sync::mpsc::Sender;
 use tokio::sync::mpsc::Receiver;
-use tokio_stream::{StreamExt, empty};
 use crate::Engine;
 use crate::EngineError;
-use crate::EngineCloseReason;
 use crate::EngineStateEntity;
-use crate::MessageData;
 use crate::TransportConfig;
 use crate::client::EngineIOClient;
 use crate::engine::{Sid, Payload, EngineInput, EngineIOClientCtrls, EngineIOServerCtrls, IO} ;
 use crate::server::EngineIOServer;
-use crate::handler::ConnectionMessage;
-use crate::transport::TransportKind;
 
-
-
-
-enum IOError<T> {
+pub enum IOError<T> {
+    InternalError,
     SendError(T),
     TransportError(T)
 }
 
-trait Transport { 
-    async fn get_stream(sid:Option<Sid>) -> Result<impl tokio_stream::Stream<Item = Payload>,IOError>;
-    async fn send(m:Payload) -> Result<(),IOError>;
+pub enum IOCloseReason {}
+
+use tokio::sync::oneshot;
+use tokio::sync::mpsc;
+
+pub(crate) type AsyncDownStreamIOPair = (
+    mpsc::Sender<(Payload, oneshot::Sender<Result<(),IOError<Payload>>>)>,
+    mpsc::Receiver<(Result<Payload,IOCloseReason>, oneshot::Sender<Result<(),EngineError>>)>
+    );
+
+pub(crate) type AsyncDownStreamIOPairOther = (
+    mpsc::Receiver<(Payload, oneshot::Sender<Result<(),IOError<Payload>>>)>,
+    mpsc::Sender<(Result<Payload,IOCloseReason>, oneshot::Sender<Result<(),EngineError>>)>
+    );
+
+type AsyncUpStreamIOPair = (
+    mpsc::Sender<Result<Payload,IOCloseReason>>, // change to engine close reason
+    mpsc::Receiver<(Payload, oneshot::Sender<Result<(),EngineError>>)>
+    );
+
+type AsyncUpStreamIOPairOTHER = (
+    mpsc::Receiver<Result<Payload,IOCloseReason>>, // change to engine close reason
+    mpsc::Sender<(Payload, oneshot::Sender<Result<(),EngineError>>)>
+    );
+
+pub trait Transport { 
+    fn connect(&self) -> AsyncDownStreamIOPair;
 }
 
-enum IOCloseReason {}
-struct FOOBAR {}
-
-
-// SO we return from the task UNSENT 
-fn create_async_clientio() -> (FOOBAR, impl tokio_stream::Stream<Item = Result<Payload,IOCloseReason>>) {
-
-    let (down_send_tx, down_send_rx) = tokio::sync::mpsc::channel(1);
-    let (down_revc_tx, down_recv_rx) = tokio::sync::mpsc::channel(1);
+pub fn setup_client_io(t:impl Transport) -> AsyncUpStreamIOPairOTHER {
     let (up_send_tx, up_send_rx) = tokio::sync::mpsc::channel(1);
-    let (up_revc_tx, up_recv_rx) = tokio::sync::mpsc::channel(1);
+    let (up_recv_tx, up_recv_rx) = tokio::sync::mpsc::channel(1);
+
+    // TODO: We need to return used engine so people can drain?
+    let task = setup_io(t.connect(), (up_send_tx, up_recv_rx));
+    return (up_recv_tx, up_send_rx);
+}
+
+async fn setup_io(
+    down_stream:AsyncDownStreamIOPair,
+    up_stream:AsyncUpStreamIOPair
+) -> Engine {
+    let (down_send, down_recv) = down_stream;
+    let (up_send, up_recv) = up_stream;
 
     let mut engine = Engine::new(EngineIOClient::new(tokio::time::Instant::now()));
     let config = TransportConfig::default();
@@ -57,15 +76,24 @@ fn create_async_clientio() -> (FOOBAR, impl tokio_stream::Stream<Item = Result<P
                     Some(IO::Wait(d)) => {
                         break Ok(Some(d))
                     }
-                    Some(_) => {
+                    Some(IO::Send(p)) => {
                         let (res_tx, res_rx) = tokio::sync::oneshot::channel::<Option<(IOError, Payload)>>();
-                        if let Err(e) = down_send_tx.send((res_tx,Payload::Ping)).await {
+                        if let Err(e) = down_send.send((res_tx,Payload::Ping)).await {
                             break Err(IOError::SendError(e.0))
                         }
-                        if let Some((err,p)) = res_rx {
-                            break Err(IOError::TransportError(p))
+                        match res_rx.await {
+                            Err(e) => break Err(IOError::InternalError),
+                            Ok(Err(e)) => break Err(e),
+                            Ok(_) => continue 
                         }
                     },
+                    Some(IO::Recv(p)) => {
+                        // We don't mind if upstream has dropped
+                        let _ = up_send.send(Ok(p)).await;
+                    }
+                    Some(_) => {
+                        // we ingore others for now ...
+                    }
                     None => {
                         break(Ok(None))
                     }
@@ -76,28 +104,42 @@ fn create_async_clientio() -> (FOOBAR, impl tokio_stream::Stream<Item = Result<P
                 Ok(None) => break, // engine closed 
                 Ok(Some(d)) => Some(d),
                 Err(e) => {
+                    // TODO: Send errors back into the engine....
+                    // HOW do we input error? CTRLs 
                     engine.input(e, now, &config);
-                    None
                 }
             };
-            let input = select! {
-                _ = tokio::time::sleep_until(next_deadline.unwrap_or(tokio::time::Instant::now())) => None,
-                s = up_recv_rx.recv() => s,
-                r = down_recv_rx.recv() => r 
+            select! {
+                _ = tokio::time::sleep_until(next_deadline.unwrap_or(tokio::time::Instant::now())) => {},
+                up = up_recv.recv() => {
+                    // TODO: dropping channel cleanup 
+                    // We report invalid input to local sender
+                    let Some((p,res_tx)) = up else { 
+                        // IF upstream closes... what do do ?
+                        break
+                    };
+                    res_tx.send(engine.input(p,now,&config)).await;
+                },
+                down = down_recv.recv() => {
+                    // TODO: dropping channel cleanup 
+                    // We report invalid input to remote sender
+                    let Some((p,res_tx)) = down else { 
+                        // when down stream closes, we can break right away
+                        // and let owner drain ...
+                        break 
+                    };
+                    res_tx.send(engine.input(p,now,&config)).await;
+                    // Data - command to show stuff 
+                    // POLL -> command from other side...
+                    // New -> command from other side as well
+                    // Close -> THIS
+                    // data stream has close in it, close at this level is for server,client??;
+                }
             };
-            if let Some((i,res_tx)) = input {
-                res_tx.send(engine.input(i, now, &config)).await;
-            }
         };
+        engine
     });
 
-
-    // output is rx Stream
-    // it can drop and we don't care?
-    // session holds tasks, so drop =kill 
-
-
-
 }
 
 
@@ -108,29 +150,6 @@ fn create_async_clientio() -> (FOOBAR, impl tokio_stream::Stream<Item = Result<P
 
 
 
-// =============================================
-
-#[derive(Debug, Clone)]
-pub struct AsyncSessionClientHandle {
-    input_tx: Sender<(Sid, EngineInput<EngineIOClientCtrls>, AsyncInputSender)>,
-}
-
-impl AsyncSessionClientHandle {
-    pub async fn input(&self, id:Sid, input:EngineInput<EngineIOClientCtrls>) -> Result<Option<impl Stream<Item =Payload>>,EngineError> {
-        let (tx,rx) = tokio::sync::oneshot::channel::<AsyncInputResult>();
-        let _ = self.input_tx.send((id,input, tx)).await;
-        let res = rx.await.unwrap_or(Err(EngineError::AlreadyClosed))?;
-        Ok(res.map(|r| tokio_stream::wrappers::ReceiverStream::new(r)))
-    }
-
-    pub async fn input_with_response_stream(&self, id:Sid, input:EngineInput<EngineIOClientCtrls>) -> Result<impl Stream<Item=Payload>,EngineError> {
-        match self.input(id, input).await {
-            Err(e) => Err(e),
-            Ok(Some(s)) => Ok(s),
-            Ok(None) => Err(EngineError::Generic)
-        }
-    }
-}
 
 // =============================================
 
@@ -147,59 +166,6 @@ pub enum Either<A,B> {
 
 type EngineServerInput = Either<EngineInput<EngineIOServerCtrls>, EngineInput<EngineIOClientCtrls>>;
 
-pub fn create_async_io2<F>(client:F, config:TransportConfig) -> AsyncSessionClientHandle 
-where F:AsyncConnectionService + 'static + Send
-{
-
-    let (client_send_tx, mut client_send_rx) = tokio::sync::mpsc::channel::<(Sid, EngineInput<EngineIOClientCtrls>, AsyncInputSender)>(1024);
-    let (server_send_tx, mut server_send_rx) = tokio::sync::mpsc::channel::<(Sid, EngineInput<EngineIOServerCtrls>, AsyncInputSender)>(1024);
-    let mut workers: HashMap<Sid,Sender<(EngineServerInput, AsyncInputSender)>> = HashMap::new();
-    
-    tokio::spawn( async move {
-        let config = config;
-        loop {
-            let (sid,input,res_tx) = tokio::select! {
-                Some(server) = server_send_rx.recv() => (server.0, Either::A(server.1), server.2),
-                Some(client) = client_send_rx.recv() => (client.0, Either::B(client.1), client.2),
-            };
-
-            let worker = match input {
-                Either::B(EngineInput::Control(EngineIOClientCtrls::New(..))) => {
-                    // Channel for IO_DISPATCHER to talk to worker
-                    let (worker_recv_tx, worker_recv_rx) = tokio::sync::mpsc::channel::<(EngineServerInput,AsyncInputSender)>(32);
-                    workers.insert(sid, worker_recv_tx.clone());
-                    // Channel for END_CLIENT to recv events 
-                    let (server_recv_tx, server_recv_rx) = tokio::sync::mpsc::channel::<Payload>(10);
-                    client.new_connection(
-                        create_connection_stream(tokio_stream::wrappers::ReceiverStream::new(server_recv_rx)),
-                        AsyncSessionServerHandle{ sid:sid.clone(), input_tx:server_send_tx.clone()  }
-                    );
-
-                    tokio::spawn(create_worker(sid, worker_recv_rx, server_recv_tx.clone(), config.clone()));
-                    Some(worker_recv_tx)
-                },
-
-                _ => {
-                    workers.get(&sid).map(|t| t.to_owned())
-                }
-            };
-
-            if let Some(w) = worker {
-               if let Err(e) = w.send_timeout((input,res_tx), Duration::from_secs(1)).await {
-                   //TODO: HOW do we time this out?
-                    //res_tx.send(Result::Err(EngineError::Generic));
-               }
-            }
-            else {
-                res_tx.send(Result::Err(EngineError::MissingSession));
-            }
-        }
-    });
-
-    return AsyncSessionClientHandle { 
-        input_tx: client_send_tx
-    }
-}
 
 async fn create_worker(id:Sid, mut rx:tokio::sync::mpsc::Receiver<(EngineServerInput,AsyncInputSender)>, tx: tokio::sync::mpsc::Sender<Payload>, config:TransportConfig) {
 
@@ -262,11 +228,6 @@ async fn create_worker(id:Sid, mut rx:tokio::sync::mpsc::Receiver<(EngineServerI
         if let Some(nd) = next_deadline  { next_tick = nd; }
         else { break } // finished
     }
-}
-
-
-pub fn create_async_client(service:impl AsyncConnectionService +'static + Send) {
-
 }
 
 
