cc2839fbc48f6afdc247fdb729c05caafc518783
Ashley Arthur <ashleyarthur98@gmail.com>
2024-03-22 09:07:05 +0000

    update server adapter

--- engineio/src/adapters/actix.rs
+++ engineio/src/adapters/actix.rs
@@ -1,8 +1,11 @@
+use tokio::time::Instant;
 use tokio_stream::StreamExt;
-use actix_web::{guard, web, HttpResponse, Resource};
-use crate::proto::{Sid, Payload, PayloadDecodeError, MessageData};
+use actix_web::{guard, web, HttpResponse, Resource, Either, ResponseError};
+use crate::io::{self, SessionCloseReason, create_session_local};
+use crate::proto::{Sid, Payload, PayloadDecodeError, MessageData, TransportConfig};
+use crate::server::EngineIOServer;
 use crate::transport::{TransportKind};
-use crate::engine::EngineError;
+use crate::engine::{EngineError, self, Engine, EngineIOClientCtrls, EngineInput};
 
 
 #[derive(serde::Deserialize)]
@@ -44,127 +47,136 @@ impl actix_web::ResponseError for EngineError {
 
 }
 
-//pub fn socket_io<F>(path:actix_web::Resource, config:TransportConfig, callback: F) -> Resource
-//where F: ConnectionService + 'static + Send + Sync 
-//{
-//    let io = create_async_io2(callback, config);
-//    
-//    // WS 
-//    let path = {
-//        let io = io.clone();
-//        path.route(
-//            web::route()
-//            .guard(guard::Get())
-//            .guard(guard::fn_guard(|ctx| {
-//                ctx.head().headers().get("Upgrade").is_some_and(|v| v == "websocket")
-//            }))
-//            .to(move |req: actix_web::HttpRequest, body: web::Payload| { 
-//                let io = io.clone();
-//                let (response, mut session, mut msg_stream) = actix_ws::handle(&req, body).unwrap();
-//                async move {
-//                    let sid = uuid::Uuid::new_v4();
-//                    let mut client_stream = io.input_with_response_stream(
-//                        sid,
-//                        EngineInput::Control(EngineIOClientCtrls::New(TransportKind::Continuous))
-//                    ).await?;
-//
-//                    actix_rt::spawn(async move {
-//                        while let Some(Ok(m)) = msg_stream.next().await {
-//                            dbg!(&m);
-//                            let _ = io.input(sid, EngineInput::Data(m.try_into())).await;
-//                        }
-//                        dbg!("end");
-//                    });
-//                    actix_rt::spawn(async move {
-//                        while let Some(p) = client_stream.next().await {
-//                            dbg!(&p);
-//                            let d = p.encode(TransportKind::Continuous);
-//                            let _ = match p {
-//                                Payload::Message(MessageData::Binary(..)) => session.binary(d).await,
-//                                _ => session.text(String::from_utf8(d).unwrap()).await
-//                            };
-//                        }
-//                        dbg!();
-//                        let _ = session.close(None).await;
-//                    });
-//                    return Ok::<HttpResponse, EngineError>(response);
-//                }
-//            })
-//        )
-//    };
-//
-//    // LONG POLL GET 
-//    let path = { 
-//        let io = io.clone();
-//        path.route(
-//            web::route()
-//            .guard(guard::Get())
-//            .guard(guard::fn_guard(|ctx| {
-//                ctx.head().uri.query().is_some_and(|s| s.contains("sid"))
-//            }))
-//            .to(move |session: web::Query<SessionInfo>| { 
-//                let io = io.clone();
-//                async move {
-//                    let sid = session.sid.ok_or(EngineError::MissingSession)?;
-//                    let s = io.input_with_response_stream(sid, EngineInput::Control(EngineIOClientCtrls::Poll)).await?;
-//                    let all = s.collect::<Vec<Payload>>().await;
-//                    Ok::<HttpResponse,EngineError>(HttpResponse::Ok().body(Payload::encode_combined(&all, TransportKind::Poll)))
-//                }
-//            })
-//        )
-//    };
-//
-//    // NEW LONG POLL
-//    let path = {
-//        let io = io.clone();
-//        path.route(
-//            web::route()
-//            .guard(guard::Get())
-//            .to(move | _session: web::Query<SessionInfo>| { 
-//                let io = io.clone();
-//                async move {
-//                    let sid = uuid::Uuid::new_v4();
-//                    let s = io.input_with_response_stream(sid, EngineInput::Control(EngineIOClientCtrls::New(crate::TransportKind::Poll))).await?;
-//                    let all = s.take(1).collect::<Vec<Payload>>().await;
-//                    let combined = Payload::encode_combined(&all, TransportKind::Poll);
-//                    Ok::<HttpResponse,EngineError>(HttpResponse::Ok().body(combined))
-//                }
-//            }
-//            )
-//        )
-//    };
-//
-//    // POST MSG LONG POLL
-//    let path = { 
-//        let io = io.clone();
-//        path.route(
-//            web::route()
-//            .guard(guard::Post())
-//            .to(move |session: web::Query<SessionInfo>, body:web::Bytes| { 
-//                let io = io.clone();
-//                async move { 
-//                    let sid = session.sid.ok_or(EngineError::MissingSession)?;
-//                    for p in Payload::decode_combined(body.as_ref(), TransportKind::Poll) {
-//                        if let Err(e) = io.input(sid, EngineInput::Data(p)).await {
-//                            return Err(e);
-//                        }
-//                    }
-//                    Ok(Ok::<HttpResponse, EngineError>(HttpResponse::Ok().body("ok")))
-//                }
-//            }
-//            )
-//        )
-//    };
-//    
-//    // Catch all - to appease clients expecting 400 not 404
-//    let path = {
-//        path.route(
-//            web::route()
-//            .to(move | _session: web::Query<SessionInfo>| {
-//                HttpResponse::BadRequest()
-//            })
-//        )
-//    };
-//    return path
-//}
+fn engine_io(path:actix_web::Resource, config:TransportConfig) -> Resource {
+    let polling = io::create_multiplex();
+
+    let path = { 
+        path.route(
+            web::route()
+            .guard(guard::Get())
+            .guard(guard::fn_guard(|ctx| {
+                ctx.head().headers().get("Upgrade").is_some_and(|v| v == "websocket")
+            }))
+            .to(move |req: actix_web::HttpRequest, body: web::Payload| { 
+                let (response, mut session, mut msg_stream) = actix_ws::handle(&req, body).unwrap();
+                async move {
+                    let sid = uuid::Uuid::new_v4();
+                    let engine = Engine::new(EngineIOServer::new(sid,Instant::now().into()));
+                    let session = create_session_local(engine, |tx,rx| {
+                        async move {
+                            loop {
+                                tokio::select! {
+                                    recv = msg_stream.next() => {
+                                        let Some(r) = recv else { break };
+                                        let res = tokio::sync::oneshot::channel();
+                                        let input  = match r {
+                                            Ok(m) => EngineInput::Data(m.try_into()),
+                                            Err(e) => EngineInput::Control(EngineIOClientCtrls::Close)
+                                        };
+                                        let s = tx.send((input,res.0)).await;
+                                    },
+                                    send = rx.recv() => {
+                                        let Some(s) = send else { break };
+                                        let d = s.encode(TransportKind::Continuous);
+                                        let _ = match s {
+                                            Payload::Message(MessageData::Binary(..)) => session.binary(d).await,
+                                            _ => session.text(String::from_utf8(d).unwrap()).await
+                                        };
+                                    }
+                                } 
+                            }
+                            //TODO 
+                            SessionCloseReason::Unknown
+                        }
+                    } );
+                    return Ok::<HttpResponse, EngineError>(response);
+                }
+            })
+        )
+    };
+
+    // LONG POLL CREATE
+    let path = {
+        let polling = polling.clone();
+        path.route(
+            web::route()
+            .guard(guard::Get())
+            .to(move | _session: web::Query<SessionInfo>| {
+                let polling = polling.clone();
+                async move {
+                    let sid = Sid::new_v4();
+                    let session = polling.create(sid).await;
+                    // DO something with session
+                    match polling.listen(sid).await {
+                        Ok(vec) => HttpResponse::Ok().body(Payload::encode_combined(&vec, TransportKind::Poll)),
+                        Err(e) => e.error_response()
+                    }
+                }
+            })
+        )
+    };
+
+    // LONG POLL GET 
+    let path = { 
+        let polling = polling.clone();
+        path.route(
+            web::route()
+            .guard(guard::Get())
+            .guard(guard::fn_guard(|ctx| {
+                ctx.head().uri.query().is_some_and(|s| s.contains("sid"))
+            }))
+            .to(move |session: web::Query<SessionInfo>| { 
+                let polling = polling.clone();
+                async move {
+                    let Some(sid) = session.sid else {
+                        return EngineError::MissingSession.error_response()
+                    };
+                    if let Err(err) = polling.input(sid, engine::EngineInput::Control(EngineIOClientCtrls::Poll)).await {
+                        return (EngineError::MissingSession).error_response()
+                    }
+                    match polling.listen(sid).await {
+                        Ok(vec) => HttpResponse::Ok().body(Payload::encode_combined(&vec, TransportKind::Poll)),
+                        Err(e) => e.error_response()
+                    }
+                }
+            })
+        )
+    };
+
+    // POST MSG LONG POLL
+    let path = { 
+        let polling = polling.clone();
+        path.route(
+            web::route()
+            .guard(guard::Post())
+            .to(move |session: web::Query<SessionInfo>, body:web::Bytes| { 
+                let polling = polling.clone();
+                async move { 
+                    let Some(sid) = session.sid else {
+                        return EngineError::MissingSession.error_response()
+                    };
+                    for p in Payload::decode_combined(body.as_ref(), TransportKind::Poll) {
+                        // TODO: Handle errors please
+                        polling.input(sid, EngineInput::Data(p)).await;
+                    }
+                    HttpResponse::Ok().finish()
+                }
+            }
+            )
+        )
+    };
+    
+    // Catch all - to appease clients expecting 400 not 404
+    let path = {
+        path.route(
+            web::route()
+            .to(move | _session: web::Query<SessionInfo>| {
+                HttpResponse::BadRequest()
+            })
+        )
+    };
+
+    return path
+}
+
 
--- engineio/src/io.rs
+++ engineio/src/io.rs
@@ -1,16 +1,21 @@
+use std::collections::HashMap;
 use std::fmt::Debug;
 use std::task::Poll;
 use futures_util::Future;
 use futures_util::Stream;
 use tokio::select;
+use tokio::time::Instant;
+use crate::engine::EngineIOClientCtrls;
 use crate::proto::PayloadDecodeError;
 use crate::proto::Payload;
+use crate::proto::Sid;
 use crate::proto::TransportConfig;
 use crate::engine::EngineInput;
 use crate::engine::Engine;
 use crate::engine::EngineError;
 use crate::engine::EngineStateEntity;
 use crate::engine::IO;
+use crate::server::EngineIOServer;
 use std::pin::Pin;
 use tokio::sync::oneshot;
 use tokio::sync::mpsc;
@@ -27,11 +32,11 @@ pub enum IOError<T> {
     TransportError(T)
 }
 pub enum IOCloseReason {}
-type EngineChannelReq<T> = (mpsc::Sender<(EngineInput<T>, oneshot::Sender<Result<(),EngineError>>)>, mpsc::Receiver<Payload>);
-type EngineChannelRes<T> = (mpsc::Sender<Payload>, mpsc::Receiver<(EngineInput<T>, oneshot::Sender<Result<(),EngineError>>)>);
-type EngineChannelPair<T> = (EngineChannelReq<T>, EngineChannelRes<T>);
+type EngineChannelReq<T,R,E> = (mpsc::Sender<(T, oneshot::Sender<Result<(),E>>)>, mpsc::Receiver<R>);
+type EngineChannelRes<T,R,E> = (mpsc::Sender<R>, mpsc::Receiver<(T, oneshot::Sender<Result<(),E>>)>);
+type EngineChannelPair<T,R,E> = (EngineChannelReq<T,R,E>, EngineChannelRes<T,R,E>);
 
-fn engine_channel<T>() -> EngineChannelPair<T> {
+fn engine_channel<T,R,E>() -> EngineChannelPair<T,R,E> {
     let (req_tx, req_rx) = tokio::sync::mpsc::channel(1);
     let (res_tx, res_rx) = tokio::sync::mpsc::channel(1);
     return (
@@ -88,14 +93,105 @@ impl <T> Stream for Session<T> {
     }
 }
 
-pub fn plex() -> () {
-    let (tx,rx) = tokio::sync::mpsc::channel(0);
+#[derive(Clone)]
+pub struct MultiPlex {
+    tx_new: mpsc::Sender<(Sid, oneshot::Sender<Session<EngineIOClientCtrls>>)>,
+    tx_input: mpsc::Sender<(Sid,EngineInput<EngineIOClientCtrls>, oneshot::Sender<Result<(),EngineError>>)>,
+    tx_listen: mpsc::Sender<(Sid, oneshot::Sender<Result<Vec<Payload>,EngineError>>)>
+}
+
+impl MultiPlex {
+    pub async fn create(&self, sid:Sid) -> Result<Session<EngineIOClientCtrls>, EngineError> {
+        let res = oneshot::channel();
+        self.tx_new.send((sid, res.0));
+        res.1.await.map_err(|_| EngineError::Generic).map(|s| Ok(s))?
+    }
+
+    pub async fn input(&self, sid:Sid, input:EngineInput<EngineIOClientCtrls>) -> Result<(),EngineError> {
+        let res = oneshot::channel();
+        self.tx_input.send((sid, input,res.0 )).await.map_err(|_| EngineError::Generic)?;
+        res.1.await.map_err(|_| EngineError::Generic)?
+    }
+
+    pub async fn listen(&self, sid:Sid) -> Result<Vec<Payload>,EngineError> {
+        let res = oneshot::channel();
+        self.tx_listen.send((sid, res.0));
+        res.1.await.map_err(|_| EngineError::Generic)?
+    }
+}
+
+pub fn create_multiplex() -> MultiPlex {
+    let tx_map: HashMap<Sid,mpsc::Sender<(EngineInput<EngineIOClientCtrls>,oneshot::Sender<Result<(),EngineError>>)>> = HashMap::new();
+    let rx_map: HashMap<Sid,mpsc::Receiver<Payload>>;
+    let input_new = mpsc::channel::<(Sid, oneshot::Sender<Session<EngineIOClientCtrls>>)>(0);
+    let input_data = mpsc::channel::<(Sid, EngineInput<EngineIOClientCtrls>, oneshot::Sender<Result<(),EngineError>>)>(0);
+    let input_listen = mpsc::channel::<(Sid, oneshot::Sender<Result<Vec<Payload>,EngineError>>)>(0);
 
     tokio::spawn(async move {
+        loop {
+            tokio::select! {
+                Some((sid,tx)) = input_new.1.recv() => {
+                    let engine = Engine::new(EngineIOServer::new(sid, Instant::now().into()));
+                    //let (close_tx, close_rx) = oneshot::channel();
+                    let session = create_session(engine, |tx,rx| {
+                        tx_map.insert(sid, tx);
+                        rx_map.insert(sid, rx);
+                        // TODO: return proper close 
+                        return async move {
+                            return SessionCloseReason::Unknown
+                        }
+                    });
+                    //tx.send(session);
+                },
+                Some((sid,data,tx)) = input_data.1.recv() => {
+                    let Some(t) = tx_map.get(&sid) else { 
+                        tx.send(Err(EngineError::Generic));
+                        continue
+                    };
+                    // Don't block main loop
+                    tokio::spawn(t.send((data,tx)));
+                },
+                Some((sid,tx)) = input_listen.1.recv() => {
+                    let Some(r) = rx_map.get(&sid) else { 
+                        tx.send(Err(EngineError::Generic));
+                        continue
+                    };
+                    tokio::spawn(async move {
+                        let mut res:Vec<Payload> = vec![];
+                        let Some(first) = r.recv().await else { 
+                            tx.send(Err(EngineError::Generic));
+                            return
+                        };
+                        res.push(first);
+                        loop {
+                            tokio::select! {
+                                _ = tokio::time::sleep(tokio::time::Duration::from_millis(100)) => {},
+                                output = r.recv() => {
+                                    match output {
+                                        None => break,
+                                        Some(o) => res.push(o)
+                                    }
+                                }
+                            }
+                        }
+                        tx.send(Ok(res));
+                    });
+                },
+                else => break
+            }
+        }
 
     });
 
+    return MultiPlex {
+        tx_input: input_data.0,
+        tx_listen: input_listen.0,
+        tx_new: input_new.0
+    }
+    
 }
+
+
 
 
 pub fn create_session<T,Fut>(
@@ -106,10 +202,9 @@ where Fut: Future<Output = SessionCloseReason> + Send,
       T:EngineStateEntity + Send + Unpin,
       T::Receive: Send,
       T::Send: Send
-      
 {
-    let (up_req, up_res) = engine_channel::<T::Send>();
-    let (down_req, down_res) = engine_channel::<T::Receive>();
+    let (up_req, up_res) = engine_channel();
+    let (down_req, down_res) = engine_channel();
     let t = transport(down_req.0, down_req.1);
 
     let handle = tokio::spawn(async move {
@@ -125,7 +220,38 @@ where Fut: Future<Output = SessionCloseReason> + Send,
     return Session { handle, tx:up_req.0, rx:up_req.1 };
 }
 
-async fn bind_engine<T:EngineStateEntity>(mut engine:Engine<T>, down_stream:EngineChannelRes<T::Receive>, up_stream:EngineChannelRes<T::Send>) -> Engine<T> {
+// TODO: Can we refactor this ??
+pub fn create_session_local<T,Fut>(
+    engine: Engine<T>, 
+    transport: impl FnOnce(mpsc::Sender<(EngineInput<T::Receive>, oneshot::Sender<Result<(),EngineError>>)>,mpsc::Receiver<Payload>) -> Fut
+) -> Session<T::Send>
+where Fut: Future<Output = SessionCloseReason>,
+      T:EngineStateEntity + Send + Unpin,
+      T::Receive: Send,
+      T::Send: Send
+{
+    let (up_req, up_res) = engine_channel();
+    let (down_req, down_res) = engine_channel();
+    let t = transport(down_req.0, down_req.1);
+
+    let handle = tokio::task::spawn_local(async move {
+        let e = tokio::spawn(bind_engine(engine, down_res, up_res));
+        let t = tokio::task::spawn_local(t);
+        tokio::select! {
+            t = &mut t => SessionCloseReason::Unknown,
+            engine = &mut t=> SessionCloseReason::Unknown
+        }
+        // HERE we should drain the engine as needed ...
+    });
+
+    return Session { handle, tx:up_req.0, rx:up_req.1 };
+}
+
+async fn bind_engine<T:EngineStateEntity>(
+    mut engine:Engine<T>,
+    down_stream:EngineChannelRes<EngineInput<T::Receive>,Payload,EngineError>,
+    up_stream:EngineChannelRes<EngineInput<T::Send>,Payload,EngineError>) -> Engine<T>
+{
     let (down_send, mut down_recv) = down_stream;
     let (up_send, mut up_recv) = up_stream;
     let config = TransportConfig::default();
