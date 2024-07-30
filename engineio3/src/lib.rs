8cbbabcf8ed96656a1253e61149ca308adc6e92e
Ashley Arthur <ashleyarthur98@gmail.com>
2024-07-30 09:17:03 +0100

    semi working wasm impl

--- engineio3/src/lib.rs
+++ engineio3/src/lib.rs
@@ -4,17 +4,34 @@ use serde_repr::*;
 
 pub struct Error {}
 
-pub enum MessageType{
-    Text,
-    Binary
+// ===============================
+pub enum Message<T>{
+    Text(T),
+    Binary(T)
+}
+impl<T> Message<T> {
+    pub fn value(self) -> T {
+        match self {
+            Message::Text(t) => t,
+            Message::Binary(t) => t
+        }
+    }
 }
 
-pub enum Payload {
+pub trait RawPayload {
+    type U;
+
+    fn prefix(&self) -> u8;
+    fn body(&self) -> Option<Self::U>;
+    fn body_as_bytes(&self) -> Vec<u8>;
+}
+
+pub enum Payload<T> {
     Open(OpenData),
     Close(CloseData),
     Ping,
     Pong,
-    Msg(MessageType),
+    Msg(Message<T>),
     Upgrade,
     Noop
 }
@@ -27,16 +44,59 @@ pub struct OpenData {
 #[derive(Serialize, Deserialize)]
 pub struct CloseData(CloseReason);
 
+// ===============================
 
-impl Payload {
+impl <T> From<Message<T>> for Payload<T> {
+    fn from(value: Message<T>) -> Self {
+        Payload::Msg(value)
+    }
+}
+pub fn decode<U>(data:impl RawPayload<U = U>) -> Result<Payload<U>,Error>{
+        match data.prefix() {
+            b'0' => {
+                let v = data.body_as_bytes();
+                let e = serde_json::from_slice(v.as_slice()).map_err(|_|Error{})?;
+                Ok(Payload::Open(e))
+            },
+            b'1' => {
+                let v = data.body_as_bytes();
+                let e = serde_json::from_slice(v.as_slice()).map_err(|_|Error{})?;
+                Ok(Payload::Close(e))
+            },
+            b'2' => Ok(Payload::Ping),
+            b'3' => Ok(Payload::Pong),
+            b'4' => {
+                let body = data.body().ok_or(Error {})?;
+                Ok(Payload::Msg(Message::Text(body)))
+            },
+            b'5' => Ok(Payload::Upgrade),
+            b'6' => Ok(Payload::Noop),
+            _ => Err(Error {})
+        }
+}
+
+
+impl <T> Payload<T> {
+    fn prefix(&self) -> u8 {
+        match self {
+            Payload::Open(_) => b'0',
+            Payload::Close(_) => b'1',
+            Payload::Ping => b'2',
+            Payload::Pong => b'3',
+            Payload::Msg(_) => b'4',
+            Payload::Upgrade => b'5',
+            Payload::Noop => b'6'
+        }
+    }
+
     pub fn into_bytes(&self) -> Vec<u8> {
         let (a,b) = match self {
             Payload::Open(data) => (b'0', Some(serde_json::to_vec(data))),
             Payload::Close(data) => (b'1', Some(serde_json::to_vec(data))),
             Payload::Ping => (b'2', None),
             Payload::Pong => (b'3', None),
-            Payload::Msg(MessageType::Text) => (b'4', None),
-            Payload::Msg(MessageType::Binary) => (b'b', None),
+            Payload::Msg(Message::Text(_)) => (b'4', None),
+            Payload::Msg(Message::Binary(_)) => (b'b', None),
             Payload::Upgrade => (b'5', None),
             Payload::Noop => (b'6', None)
         };
@@ -45,33 +105,10 @@ impl Payload {
         if let Some(Ok(mut d)) = b {
             v.append(&mut d);
         };
-        return v
-    }
-
-    pub fn from_iter16<T>(mut value:T) -> Result<Self,Error> where T:Iterator<Item = u16> {
-        let n = value.next().ok_or(Error {})?;            
-        let b = n.to_be_bytes();
-        let data:Vec<u8> = value.map(|a| a.to_be_bytes()).flatten().collect();
-
-        match b.last().unwrap() {
-            b'0' => {
-                let e = serde_json::from_slice(data.as_slice()).map_err(|_|Error{})?;
-                Ok(Payload::Open(e))
-            },
-            b'1' => {
-                let e = serde_json::from_slice(data.as_slice()).map_err(|_|Error{})?;
-                Ok(Payload::Close(e))
-            },
-            b'2' => Ok(Payload::Ping),
-            b'3' => Ok(Payload::Pong),
-            b'4' => Ok(Payload::Msg(MessageType::Text)),
-            b'b' => Ok(Payload::Msg(MessageType::Binary)),
-            b'5' => Ok(Payload::Upgrade),
-            b'6' => Ok(Payload::Noop),
-            _ => Err(Error {})
-        }
+        v
     }
 }
+
 
 #[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug)]
 #[repr(u8)]
@@ -83,18 +120,29 @@ pub enum CloseReason {
 }
 
 
-pub struct Engine {
+pub struct Engine<T> {
+    buffer: VecDeque<Payload<T>>
 }
 
-impl Engine {
+impl<T> Default for Engine<T> {
+    fn default() -> Self {
+        Self {
+            buffer:VecDeque::new()
+        }
+    }
+}
+
+impl<T> Engine<T> {
+
+
     // We pass paylaod into engine 
     // to update state 
     // but engine doesn't own payload 
-    pub fn handle_input(&mut self, p:&Payload) -> Result<(),Error>{
+    pub fn handle_input(&mut self, p:&Payload<T>) -> Result<(),Error>{
         Ok(())
     }
 
-    pub fn poll(&mut self, now:Instant) -> Option<Payload> {
+    pub fn poll(&mut self, now:Instant) -> Option<Payload<T>> {
         None
     }
 
--- engineio_wasm/src/lib.rs
+++ engineio_wasm/src/lib.rs
@@ -1,22 +1,45 @@
-use std::rc::Rc;
 use std::time;
 
+use engineio3::RawPayload;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsValue;
 use web_sys::{ErrorEvent, MessageEvent, WebSocket};
 use gloo_timers::future::TimeoutFuture;
-use futures::{FutureExt, channel::mpsc, StreamExt, select};
-use engineio3::Payload;
+use futures::{FutureExt, channel::mpsc, channel::oneshot, StreamExt, select};
+use engineio3::{Payload, Message};
 
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = JSON, catch)]
-    fn parse(s:js_sys::JsString) -> Result<JsValue,JsValue>;
+    fn parse(s:JsValue) -> Result<JsValue,JsValue>;
 }
 
-pub fn parse_msg(s:&js_sys::JsString) -> Result<JsValue,JsValue> {
-    let ss = s.slice(1,s.length() - 1);
-    parse(ss)
+// ==========================
+struct WSTextMessage(js_sys::JsString);
+
+// We want to avoid copying the whole body into wasm 
+// just to dispatch backout...
+// so we cannot JUST pass a byte array to CORE
+// instead we pass a handle to the data 
+// but Sometimes... we do want the bytes FFS 
+// so we have both options 
+// TODO: Body should be INTO bytes
+impl RawPayload for WSTextMessage {
+    type U = JsValue;
+    fn body(&self) -> Option<Self::U> {
+        if self.0.length() == 1 { 
+            None
+        }
+        else {
+            Some(self.0.slice(1, self.0.length()).into())
+        }
+    }
+    fn body_as_bytes(&self) -> Vec<u8> {
+        self.0.iter().flat_map(|a|a.to_be_bytes()).collect()
+    }
+    fn prefix(&self) -> u8 {
+        self.0.iter().next().map(|a|a.to_be_bytes()[0]).unwrap_or(99)
+    }
 }
 
 // ==========================
@@ -28,7 +51,8 @@ pub enum JSError {
 
 #[wasm_bindgen]
 pub struct Engine {
-    transport: Rc<WebSocket>,
+    // TODO: should we support full times of WS?
+    tx: mpsc::Sender<engineio3::Message<JsValue>>, 
 }
 
 #[wasm_bindgen]
@@ -37,78 +61,111 @@ impl Engine {
         todo!()
     }
 
-    pub fn send(&self, msg:&js_sys::JsString) {
-        self.transport.send_with_array_buffer_view(msg)
+    // Send needs to transer ownership because we queue events 
+    // so cannot be stack based ref
+    // TODO: what does it mean to be mut self for export js?
+    pub fn send(&mut self, msg:js_sys::JsString) -> Result<(),JsValue> {
+        // TODO: ERROR please
+        let m:Message::<JsValue> = Message::Text(msg.into());
+        let _ = self.tx.try_send(m);
+        Ok(())
     }
 }
 
 // TODO: we should make better types for Function + return error 
+// on msg should be an enum : connection || message 
 #[wasm_bindgen]
-pub fn create_ws_engine(url:&str, on_msg:js_sys::Function) -> Result<Engine, JsValue> {
+pub async fn create_ws_engine(url:&str, on_msg:js_sys::Function) -> Result<Engine, JsValue> {
+    let (mut ingress_tx, ingress_rx) = mpsc::channel(64);
+    let (egress_tx, egress_rx) = mpsc::channel(64);
+
     let ws = WebSocket::new(url)?;
     ws.set_binary_type(web_sys::BinaryType::Arraybuffer);
-
-    let (mut ingress_tx, ingress_rx) = mpsc::channel(64);
-    let (mut egress_tx, egress_rx) = mpsc::channel(64);
-    let f = Closure::wrap(Box::new(move |v:MessageEvent| {
-        let _ = ingress_tx.try_send(v);
-    }) as Box<dyn FnMut(MessageEvent)>);
-    
-    ws.set_onmessage(Some(f.as_ref().unchecked_ref()));
-    // TODO: Better please
-    f.forget();
-
-    let ws_shared = Rc::new(ws);
-
-    let e = Engine { transport: ws_shared.clone() };
+    let e = Engine { tx: egress_tx };
     wasm_bindgen_futures::spawn_local(async move {
-        let ee = engineio3::Engine { };
-        ws_poll(ee, ingress_rx, egress_rx, ws_shared.as_ref(), on_msg).await;
+        let f = Closure::wrap(Box::new(move |v:MessageEvent| {
+            let _ = ingress_tx.try_send(v);
+        }) as Box<dyn FnMut(MessageEvent)>);
+        ws.set_onmessage(Some(f.as_ref().unchecked_ref()));
+
+        let state = engineio3::Engine::<JsValue>::default();
+        ws_poll(state, ingress_rx, egress_rx, &ws, on_msg).await;
+        // close ws so callback is not called again
+        let _ = ws.close();
     });
+
+    // TOOD: we should oneshot and let them know when ws_poll exits
     Ok(e)
 }
 
 // MAIN Event Loop
 async fn ws_poll(
-    mut e:engineio3::Engine,
+    mut e:engineio3::Engine<JsValue>,
     mut ingress_rx:mpsc::Receiver<MessageEvent>, 
-    mut egress_rx:mpsc::Receiver<MessageEvent>, 
+    mut egress_rx:mpsc::Receiver<Message<JsValue>>, 
     ws: &WebSocket,
     callback:js_sys::Function
     )
 {
     loop {
         let now = time::Instant::now();
-        while let Some(p) = e.poll(now) {
-            let data = p.into_bytes();
-            let err = match ws.ready_state() {
-                web_sys::WebSocket::OPEN => {
-                    ws.send_with_u8_array(data.as_slice()).map_err(|_| JSError::TransportError)
-                },
-                _ => Err(JSError::TransportError)
-            };
 
+        // Send out any pending payloads 
+        // either messages or protocol specifics
+        while let Some(p) = e.poll(now) {
+            if ws.ready_state() != web_sys::WebSocket::OPEN {
+                break
+            }
+            let err = match p {
+                Payload::Msg(Message::Text(m)) => {
+                    // TODO: HACK! yolo
+                    let o:js_sys::Object = m.unchecked_into();
+                    // TODO:Hpw to prefix...
+                    ws.send_with_array_buffer_view(&o).map_err(|_| JSError::TransportError)
+                },
+                Payload::Msg(Message::Binary(m)) => {
+                   // TODO:
+                   Ok(())
+                },
+                _ => {
+                    // For non Message, we own the data so serialise please
+                    let data = p.into_bytes();
+                    ws.send_with_u8_array(data.as_slice()).map_err(|_| JSError::TransportError)
+                }
+            };
+            // IF any sending fails, close out
             if let Err(_e) = err {
                 // Close down transport 
                 break 
             }
-
         }
+
+        // receive new input + timeout
         let Some(t) = e.next_timeout() else { 
             break
         };
 
         let mut timeout = TimeoutFuture::new(t.as_millis() as u32).fuse();
-        let mut next = ingress_rx.next().fuse();
+        let mut next_in = ingress_rx.next().fuse();
+        let mut next_out = egress_rx.next().fuse();
 
-        // TODO: move sending here via chan so we can fully own ws
         select! {
-            v = next => {
-                if v.is_none() { break } 
+            m = next_out => {
+                // JS side has dropped sender... lets drop ? 
+                if m.is_none() { break } 
+                // Error if engine is closed etc
+                let Ok(_) = e.handle_input(&m.unwrap().into()) else {
+                    // protocol broken! assume worse and close down engine
+                    break
+                };
+            },
 
+            v = next_in => {
+                if v.is_none() { break } 
                 // TODO: what about binary
                 let Ok(txt) = v.unwrap().data().dyn_into::<js_sys::JsString>() else { continue };
-                let Ok(p) = Payload::from_iter16(&mut txt.iter()) else { continue };
+                //let Ok(p) = Payload::from_iter16(&mut txt.iter()) else { continue };
+                let Ok(p) = engineio3::decode(WSTextMessage(txt)) else { continue } ;
 
                 // We feed engine to update state 
                 // state being + socket connection + business logic 
@@ -117,12 +174,21 @@ async fn ws_poll(
                     break
                 };
 
-                // we return any msg payloads to listeners
-                if let Payload::Msg(_t) = p {
-                    if let Ok(data) = parse(txt) {
-                        let _ = callback.call1(&data, &data);
+                match p {
+                    Payload::Msg(Message::Text(v)) => {
+                        if let Ok(data) = parse(v) {
+                            // We pass reference back to JS... but what happens to ownership??
+                            // JS will see the real value assumedly ( the real value lives in JS!)
+                            // so normal js ref count on object
+                            // the handle stays Rust side ... and dies normally ( assumedly ... ) 
+                            let _ = callback.call1(&JsValue::NULL, &data);
+                        }
                     }
-                };
+                    Payload::Msg(Message::Binary(v)) => {
+                        let _ = callback.call1(&JsValue::NULL,&v);
+                    }
+                    _ => {}
+                }
             }
             _ = timeout => {}
         };
